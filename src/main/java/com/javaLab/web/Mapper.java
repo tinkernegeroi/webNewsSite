a5758a2de64d048f45bde6546f43f6a6932a1675
Roman <roman.peskoff@gmail.com>
2025-11-20 01:50:22 +0300

    add Mapper

---
+++ src/main/java/com/javaLab/web/Mapper.java
@@ -0,0 +1,57 @@
+package com.javaLab.web;
+
+import com.javaLab.web.dto.UserCreateDTO;
+import com.javaLab.web.dto.UserResponseDTO;
+import com.javaLab.web.models.Role;
+import com.javaLab.web.models.UserModel;
+import com.javaLab.web.exceptions.FileUploadException;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Mapper {
+    private static final Role DEFAULT_ROLE = Role.VISITOR;
+
+    private static final String IMAGE_PATH = "C:\\Users\\Roman\\IdeaProjects\\web" +
+            "\\src\\main\\resources\\static\\images";
+
+    private final static String IMAGE_URL = "http://localhost:8080/images/";
+
+    private final static String DEFAULT_IMAGE_URL = "http://localhost:8080/images/DEFAULT.png";
+
+    public static UserModel userCreateDtoToUser (UserCreateDTO userCreateDTO){
+        UserModel user = new UserModel();
+        user.setUsername(userCreateDTO.getUsername());
+        user.setPassword(userCreateDTO.getPassword());
+        user.setEmail(userCreateDTO.getEmail());
+        user.setRole(DEFAULT_ROLE);
+        user.setAvatar(processAvatar(userCreateDTO.getAvatar(), userCreateDTO.getUsername()));
+        return user;
+    }
+
+    public static String processAvatar(MultipartFile avatar, String username){
+        if (avatar != null) {
+            try {
+                String Filename = username + "_" + avatar.getOriginalFilename();
+                File file = new File(IMAGE_PATH, Filename);
+                avatar.transferTo(file);
+                return IMAGE_URL + Filename;
+            } catch (IOException e) {
+                throw new FileUploadException("Не удалось загрузить аватар");
+            }
+        }else {
+            return DEFAULT_IMAGE_URL;
+        }
+    }
+
+    public static UserResponseDTO userToUserResponseDto (UserModel user){
+        return new UserResponseDTO(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getAvatar(),
+                user.getRole()
+        );
+    }
+}

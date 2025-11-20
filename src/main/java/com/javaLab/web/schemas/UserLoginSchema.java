6a56da906074d8a9c01a9258f59d91e010ede425
tinkernegeroi <Roman.peskoff@gmail.com>
2025-11-20 20:48:27 +0300

    add UserLoginSchema

---
+++ src/main/java/com/javaLab/web/schemas/UserLoginSchema.java
@@ -0,0 +1,18 @@
+package com.javaLab.web.schemas;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginSchema {
+
+    private String username;
+
+    private String password;
+}

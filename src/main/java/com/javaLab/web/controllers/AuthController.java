81abd66c666c88548ce81cb55c21a9cbb15ccca6
Roman <roman.peskoff@gmail.com>
2025-11-21 01:24:59 +0300

    add auth controller and service

---
+++ src/main/java/com/javaLab/web/controllers/AuthController.java
@@ -0,0 +1,43 @@
+package com.javaLab.web.controllers;
+
+import com.javaLab.web.dto.UserCreateDTO;
+import com.javaLab.web.dto.UserLoginDTO;
+import com.javaLab.web.services.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(
+            @RequestParam String username,
+            @RequestParam String password,
+            @RequestParam String email,
+            @RequestParam(required = false) MultipartFile avatar
+    ){
+        UserCreateDTO dto = new UserCreateDTO(username, password, email, avatar);
+        return authService.register(dto);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(
+            @RequestBody UserLoginDTO userLoginDTO,
+            HttpSession session
+    ){
+        return authService.login(userLoginDTO, session);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request){
+        return authService.logout(request);
+    }
+}
---
+++ src/main/java/com/javaLab/web/services/AuthService.java
@@ -0,0 +1,64 @@
+package com.javaLab.web.services;
+
+import com.javaLab.web.dto.UserCreateDTO;
+import com.javaLab.web.dto.UserLoginDTO;
+import com.javaLab.web.dto.UserResponseDTO;
+import com.javaLab.web.models.User;
+import com.javaLab.web.repository.UserRepository;
+import com.javaLab.web.utils.Mapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+
+    public ResponseEntity<?> register(UserCreateDTO dto) {
+
+        Optional<String> error = checkIfUserExists(dto);
+        if (error.isPresent()) return ResponseEntity.badRequest().body(error.get());
+
+        User savedUser = userRepository.save(Mapper.userCreateSchemaToDTO(dto));
+        return ResponseEntity.ok(Mapper.userToUserResponseDTO(savedUser));
+    }
+
+    public ResponseEntity<?> login(UserLoginDTO userLoginDTO, HttpSession session) {
+        Optional<User> userOptional = userRepository.findByUsername(userLoginDTO.getUsername());
+        if(userOptional.isEmpty()){
+            return new ResponseEntity<>
+                    ("Пользователь с таким логином не зарегистрирован", HttpStatus.NOT_FOUND);
+        }
+
+        User user = userOptional.get();
+
+        if (!user.getPassword().equals(userLoginDTO.getPassword())) {
+            return new ResponseEntity<>("Неправильный пароль", HttpStatus.UNAUTHORIZED);
+        }
+        UserResponseDTO userResponseDTO = Mapper.userToUserResponseDTO(user);
+        session.setAttribute("user", userResponseDTO);
+        System.out.println(session.getAttribute("user"));
+        return ResponseEntity.ok(userResponseDTO);
+    }
+
+    public ResponseEntity<?> logout(HttpServletRequest request){
+        HttpSession session = request.getSession(false);
+        if (session != null) session.invalidate();
+        return ResponseEntity.ok("Logged out");
+    }
+
+    private Optional<String> checkIfUserExists(UserCreateDTO userCreateDTO) {
+        if (userRepository.findByUsername(userCreateDTO.getUsername()).isPresent())
+            return Optional.of("Пользователь с таким логином уже зарегистрирован");
+        if (userRepository.findByEmail(userCreateDTO.getEmail()).isPresent())
+            return Optional.of("Пользователь с такой почтой уже зарегистрирован");
+        return Optional.empty();
+    }
+}

46eab7ec9038826247a44cfdd66c7b6457a49099
tinkernegeroi <Roman.peskoff@gmail.com>
2025-11-20 20:48:47 +0300

    add loginUser

--- src/main/java/com/javaLab/web/services/UserService.java
+++ src/main/java/com/javaLab/web/services/UserService.java
@@ -1,7 +1,7 @@
 package com.javaLab.web.services;
 
 import com.javaLab.web.Mapper;
-import com.javaLab.web.dto.UserCreateDTO;
+import com.javaLab.web.schemas.UserCreateSchema;
 import com.javaLab.web.models.UserModel;
 import com.javaLab.web.repository.UserRepository;
 import lombok.AllArgsConstructor;
@@ -20,24 +20,28 @@ public class UserService {
 
     private UserRepository userRepository;
 
-    public ResponseEntity<?> registerUser(UserCreateDTO userCreateDTO) {
-        Optional<String> error = checkIfUserExists(userCreateDTO);
+    public ResponseEntity<?> registerUser(UserCreateSchema userCreateSchema) {
+        Optional<String> error = checkIfUserExists(userCreateSchema);
         if (error.isPresent()) {
             return new ResponseEntity<>(error.get(), HttpStatus.CONFLICT);
         }
-        error = validateUserInput(userCreateDTO);
+        error = validateUserInput(userCreateSchema);
         if (error.isPresent()) {
             return new ResponseEntity<>(error.get(), HttpStatus.BAD_REQUEST);
         }
-        UserModel savedUser = userRepository.save(Mapper.userCreateDtoToUser(userCreateDTO));
-        return ResponseEntity.ok(Mapper.userToUserResponseDto(savedUser));
+        UserModel savedUser = userRepository.save(Mapper.userCreateSchemaToUser(userCreateSchema));
+        return ResponseEntity.ok(Mapper.userToUserResponseSchema(savedUser));
     }
 
-    private Optional<String> checkIfUserExists(UserCreateDTO userCreateDTO) {
-        if (userRepository.findByUsername(userCreateDTO.getUsername()).isPresent()) {
+    public ResponseEntity<?> loginUser(UserLoginSchema userLoginSchema){
+        
+    }
+
+    private Optional<String> checkIfUserExists(UserCreateSchema userCreateSchema) {
+        if (userRepository.findByUsername(userCreateSchema.getUsername()).isPresent()) {
             return Optional.of("Пользователь с таким логином уже зарегистрирован");
         }
-        if (userRepository.findByEmail(userCreateDTO.getEmail()).isPresent()) {
+        if (userRepository.findByEmail(userCreateSchema.getEmail()).isPresent()) {
             return Optional.of("Пользователь с такой почтой уже зарегистрирован");
         }
         return Optional.empty();

55c54e9c805f254324d4b9db0bc38e75fcecbcea
Roman <roman.peskoff@gmail.com>
2025-11-21 01:24:35 +0300

    rename

--- src/main/java/com/javaLab/web/repository/UserRepository.java
+++ src/main/java/com/javaLab/web/repository/UserRepository.java
@@ -1,14 +1,14 @@
 package com.javaLab.web.repository;
 
-import com.javaLab.web.models.UserModel;
+import com.javaLab.web.models.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<UserModel, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<UserModel> findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
-    Optional<UserModel> findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }

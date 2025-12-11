e3f9d869bb89320ee5cdca9c2ee6ec96869b3928
Roman <roman.peskoff@gmail.com>
2025-12-11 22:42:47 +0300

    create controller

---
+++ src/main/java/com/javaLab/web/controllers/ModeratorController.java
@@ -0,0 +1,26 @@
+package com.javaLab.web.controllers;
+
+
+import com.javaLab.web.dto.NewsCreateDTO;
+import com.javaLab.web.services.ModeratorService;
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/moderator")
+@AllArgsConstructor
+public class ModeratorController {
+    private final ModeratorService moderatorService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createNews (@ModelAttribute NewsCreateDTO dto) {
+        return moderatorService.createNews(dto);
+    }
+
+
+}

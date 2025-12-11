321535e91183ce839b0429b8fd849e65b30b9ff9
Roman <roman.peskoff@gmail.com>
2025-12-11 20:27:09 +0300

    I AM A TEAPOT

--- src/main/java/com/javaLab/web/exceptions/FileUploadException.java
+++ src/main/java/com/javaLab/web/exceptions/FileUploadException.java
@@ -3,7 +3,7 @@ package com.javaLab.web.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.I_AM_A_TEAPOT)
 public class FileUploadException extends RuntimeException {
 
     public FileUploadException(String message){

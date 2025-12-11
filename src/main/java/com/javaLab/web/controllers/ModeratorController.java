f87393e43f1a230a05a2a9d4c6ab2444c40aca35
Roman <roman.peskoff@gmail.com>
2025-12-11 23:15:46 +0300

    create edit and delete for news

--- src/main/java/com/javaLab/web/controllers/ModeratorController.java
+++ src/main/java/com/javaLab/web/controllers/ModeratorController.java
@@ -3,29 +3,38 @@ package com.javaLab.web.controllers;
 
 import com.javaLab.web.dto.NewsCreateDTO;
 import com.javaLab.web.services.ModeratorService;
-import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/moderator")
+@RequestMapping("/news")
 @AllArgsConstructor
 public class ModeratorController {
     private final ModeratorService moderatorService;
 
-    @PostMapping("/create")
+    @PostMapping
     public ResponseEntity<?> createNews (@ModelAttribute NewsCreateDTO dto) {
         return moderatorService.createNews(dto);
     }
 
-    @GetMapping("/news")
-    public ResponseEntity<?> getNews(HttpSession session) {
+    @GetMapping
+    public ResponseEntity<?> getNews() {
         return moderatorService.getAllNews();
     }
 
-    @GetMapping("/news/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<?> getNewsById(@PathVariable Long id) {
         return moderatorService.getNewsById(id);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> editNews(@PathVariable Long id, @ModelAttribute NewsCreateDTO dto) {
+        return moderatorService.editNews(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteNews(@PathVariable Long id) {
+        return moderatorService.deleteNewsById(id);
     }
 }
--- src/main/java/com/javaLab/web/services/ModeratorService.java
+++ src/main/java/com/javaLab/web/services/ModeratorService.java
@@ -3,11 +3,9 @@ package com.javaLab.web.services;
 
 import com.javaLab.web.dto.NewsCreateDTO;
 import com.javaLab.web.exceptions.NewsNotFoundException;
-import com.javaLab.web.exceptions.UserNotFoundException;
 import com.javaLab.web.models.News;
 import com.javaLab.web.repository.NewsRepository;
 import com.javaLab.web.utils.Mapper;
-import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -36,5 +34,30 @@ public class ModeratorService {
     public ResponseEntity<?> getNewsById(Long id){
         News news = newsRepository.findById(id).orElseThrow(() -> new NewsNotFoundException("News not found"));
         return ResponseEntity.ok(news);
+    }
+
+    public ResponseEntity<?> editNews(Long id, NewsCreateDTO newsCreateDTO) {
+        News news = newsRepository.findById(id).orElseThrow(() -> new NewsNotFoundException("News not found"));
+
+        if (newsCreateDTO.getTitle() != null && !newsCreateDTO.getTitle().isBlank())
+            news.setTitle(newsCreateDTO.getTitle());
+
+        if (newsCreateDTO.getDescription() != null && !newsCreateDTO.getDescription().isBlank())
+            news.setDescription(newsCreateDTO.getDescription());
+
+        if (newsCreateDTO.getImage() != null && !newsCreateDTO.getImage().isEmpty()){
+            String imageSrc = mapper.processAvatar(newsCreateDTO.getImage(), newsCreateDTO.getTitle());
+            news.setImageSrc(imageSrc);
+        }
+
+        news = newsRepository.save(news);
+        return ResponseEntity.ok(news);
+    }
+
+    public ResponseEntity<?> deleteNewsById(Long id){
+        News news = newsRepository.findById(id).orElseThrow(() -> new NewsNotFoundException("News not found"));
+        mapper.deleteAvatar(news.getImageSrc());
+        newsRepository.delete(news);
+        return ResponseEntity.ok("News has been deleted");
     }
 }
